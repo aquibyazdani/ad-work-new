a85af5fdfc7a0a17162a866321278f73833b9b9f
aquibyazdani <77403878+aquibyazdani@users.noreply.github.com>
2021-05-13 17:15:57 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,10 @@
+ const data = [
+    {
+        id :1 ,
+        imgUrl : "https://lh5.googleusercontent.com/F_EdSBUFywoPjtz0rjoTGPLBDm5VPfQENidHGcS2i0f-LVe5xqfIn8hFRk9xpUi64VI2-Yzv77dt46EbcuBJS0dVNaW9xX_f4mVRMlQ6SzlrmcPBohLpFrPv19MWeWzUHjjXCb1c"
+    } 
+  
+
+]
+
+export default data;
